cf5d249da00585971a82b73d92494dd9b1941212
yu hsiang <3193+tomlin@g.users.noreply.github.com>
2025-10-17 10:47:15 +0800

    feat: add setting content

--- app/pages/setting.vue
+++ app/pages/setting.vue
@@ -1,2 +1,15 @@
-<template><div>setting</div></template>
-<script setup lang="ts"></script>
+<template>
+  <div class="intro">
+    <h2>設定</h2>
+    <p>
+      異動結果會儲存於瀏覽器內存中，無需多做額外設定。<br />因儲存於瀏覽器內存中，因此若為不同電腦使用請點擊以下備份檔案進行匯出
+      。
+    </p>
+    <div>
+      <DownloadButton />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import DownloadButton from "~/components/DownloadButton.vue";
+</script>
