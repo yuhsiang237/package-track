762f6c201d33d9c7b9f5ccecbcc5496b34f1a3b7
yuhsiang237 <12533553+yuhsiang237@users.noreply.github.com>
2025-11-03 22:12:42 +0800

    feat: add SSR flag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+console.log('SSR flag:', import.meta.env.SSR ?? false)
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   // Disable server-side rendering (SSR), generate only static pages
-  ssr: false,
+  ssr: import.meta.env.SSR ?? false,
   app: {
     // Base URL for the application.
     // On GitHub Pages, this should match the repository name
