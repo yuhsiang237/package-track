4cc85d502635a007a3f33ea9f2f71188b81e11c6
yu hsiang <3193+tomlin@g.users.noreply.github.com>
2025-11-03 11:15:17 +0800

    adjust breakpoint for mobile layout

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,27 @@
 <template>
-  <NuxtLayout>
+  <NuxtLayout :name="layout">
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const layout = ref<"default" | "mobile">("default");
+
+const updateLayout = () => {
+  layout.value = window.innerWidth < 767 ? "mobile" : "default";
+};
+
+onMounted(() => {
+  updateLayout();
+  window.addEventListener("resize", updateLayout);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", updateLayout);
+});
+</script>
+
 <style lang="scss">
 @use "~/assets/scss/main.scss" as *;
 </style>
---
+++ app/layouts/mobile.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="mobile_layout"></div>
+</template>
+<script setup lang="ts"></script>
